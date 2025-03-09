3451397ec5bf6702a165bfdc83e697f2b2c94f02
codingpalette <codingpalette@gmail.com>
2025-03-09 14:15:58 +0900

    애널리틱스 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css";
 import { TanstackProvider } from "@/src/app/providers";
 
@@ -35,6 +36,7 @@ export default function RootLayout({
         <TanstackProvider>
           {children}
         </TanstackProvider>
+        <GoogleAnalytics gaId="G-4KZV4L3ECY" />
       </body>
     </html>
   );
