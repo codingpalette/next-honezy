9cd92fb89f7489ec4304b50770911f98651ef080
codingpalette <codingpalette@gmail.com>
2025-05-19 19:03:41 +0900

    fix(페이지): 페이지 추가

---
+++ app/stats/2025/page.tsx
@@ -0,0 +1,15 @@
+
+
+
+export default function Stats2025Page() {
+  return (
+    <>
+      <div className="w-full h-full" style={{ backgroundColor: 'rgb(18, 18, 18)' }}>
+        <section className="w-full h-full">
+        </section>
+
+      </div>
+    </>
+  )
+}
+
