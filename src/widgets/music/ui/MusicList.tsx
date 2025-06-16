2652e4f46ed4d276dbc5a38bfcb655a1ae432771
codingpalette <codingpalette@gmail.com>
2025-06-17 07:11:53 +0900

    fix(music): 재생 주석

--- src/widgets/music/ui/MusicList.tsx
+++ src/widgets/music/ui/MusicList.tsx
@@ -254,22 +254,22 @@ export function MusicList() {
                       {music.time}
                     </div>
                   </div>
-                  <div>
-                    <button
-                      className="btn btn-square btn-ghost"
-                      onClick={() => handlePlay(music)}
-                    >
-                      {playingMusic?.id === music.id && playing ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
-                        </svg>
-                      ) : (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
-                        </svg>
-                      )}
-                    </button>
-                  </div>
+                  {/* <div> */}
+                  {/*   <button */}
+                  {/*     className="btn btn-square btn-ghost" */}
+                  {/*     onClick={() => handlePlay(music)} */}
+                  {/*   > */}
+                  {/*     {playingMusic?.id === music.id && playing ? ( */}
+                  {/*       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"> */}
+                  {/*         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" /> */}
+                  {/*       </svg> */}
+                  {/*     ) : ( */}
+                  {/*       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"> */}
+                  {/*         <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" /> */}
+                  {/*       </svg> */}
+                  {/*     )} */}
+                  {/*   </button> */}
+                  {/* </div> */}
                   <a href={music.link} target="_blank">
                     <button className="btn btn-square btn-ghost">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
